14e465a9a6c263071187c5e109ac8fa19f72f6fa
hashmat <hashmat@tecmavin.com>
2018-11-20 17:09:03 +0530

    page front tpl

--- sites/all/themes/custom/vivezacare/templates/page--front.tpl.php
+++ sites/all/themes/custom/vivezacare/templates/page--front.tpl.php
@@ -3,6 +3,6 @@
   $langcode = $language->language;
   include 'page.header.inc' 
 ?>
-<h1> PLEASE VISIT INNER PAGES </h1>
+<h1><?php print t('Please click on the unhighlighted menu items'); ?></h1>
 
 <?php include 'page.footer.inc' ?>
