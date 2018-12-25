90d441639c0dfcc7e5bd485cb9ae45e32680a7fb
hashmat <hashmat@tecmavin.com>
2018-12-25 13:48:58 +0530

    page tpl changes:MHA

--- sites/all/themes/custom/vivezacare/templates/page.tpl.php
+++ sites/all/themes/custom/vivezacare/templates/page.tpl.php
@@ -85,15 +85,16 @@
  * @see html.tpl.php
  */
 ?>
+<?php include 'page.header.inc'; ?>
 <div id="page-wrapper"><div id="page">
 
-  <div id="header" class="<?php print $secondary_menu ? 'with-secondary-menu': 'without-secondary-menu'; ?>"><div class="section clearfix">
+  <!-- <div id="header" class="<?php //print $secondary_menu ? 'with-secondary-menu': 'without-secondary-menu'; ?>"><div class="section clearfix"> -->
 
-    <?php if ($logo): ?>
+    <?php /* if ($logo): ?>
       <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
         <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
       </a>
-    <?php endif; ?>
+    <?php endif; */ ?>
 
     <?php if ($site_name || $site_slogan): ?>
       <div id="name-and-slogan"<?php if ($hide_site_name && $hide_site_slogan) { print ' class="element-invisible"'; } ?>>
@@ -121,9 +122,9 @@
       </div> <!-- /#name-and-slogan -->
     <?php endif; ?>
 
-    <?php print render($page['header']); ?>
+    <?php //print render($page['header']); ?>
 
-    <?php if ($main_menu): ?>
+    <?php /* if ($main_menu): ?>
       <div id="main-menu" class="navigation">
         <?php print theme('links__system_main_menu', array(
           'links' => $main_menu,
@@ -138,9 +139,9 @@
           ),
         )); ?>
       </div> <!-- /#main-menu -->
-    <?php endif; ?>
+    <?php endif; */?>
 
-    <?php if ($secondary_menu): ?>
+    <?php /* if ($secondary_menu): ?>
       <div id="secondary-menu" class="navigation">
         <?php print theme('links__system_secondary_menu', array(
           'links' => $secondary_menu,
@@ -155,7 +156,7 @@
           ),
         )); ?>
       </div> <!-- /#secondary-menu -->
-    <?php endif; ?>
+    <?php endif; */?>
 
   </div></div> <!-- /.section, /#header -->
 
@@ -165,26 +166,26 @@
     </div></div> <!-- /.section, /#messages -->
   <?php endif; ?>
 
-  <?php if ($page['featured']): ?>
+  <?php /* if ($page['featured']): ?>
     <div id="featured"><div class="section clearfix">
       <?php print render($page['featured']); ?>
     </div></div> <!-- /.section, /#featured -->
-  <?php endif; ?>
+  <?php endif; */?>
 
   <div id="main-wrapper" class="clearfix"><div id="main" class="clearfix">
 
-    <?php if ($breadcrumb): ?>
+    <?php /*if ($breadcrumb): ?>
       <div id="breadcrumb"><?php print $breadcrumb; ?></div>
-    <?php endif; ?>
+    <?php endif; */?>
 
-    <?php if ($page['sidebar_first']): ?>
+    <?php /*if ($page['sidebar_first']): ?>
       <div id="sidebar-first" class="column sidebar"><div class="section">
         <?php print render($page['sidebar_first']); ?>
       </div></div> <!-- /.section, /#sidebar-first -->
-    <?php endif; ?>
+    <?php endif; */?>
 
     <div id="content" class="column"><div class="section">
-      <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
+      <?php /*if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
       <?php if ($title): ?>
@@ -203,45 +204,47 @@
         <ul class="action-links">
           <?php print render($action_links); ?>
         </ul>
-      <?php endif; ?>
+      <?php endif; */?>
       <?php print render($page['content']); ?>
       <?php print $feed_icons; ?>
 
     </div></div> <!-- /.section, /#content -->
 
-    <?php if ($page['sidebar_second']): ?>
+    <?php /*if ($page['sidebar_second']): ?>
       <div id="sidebar-second" class="column sidebar"><div class="section">
         <?php print render($page['sidebar_second']); ?>
       </div></div> <!-- /.section, /#sidebar-second -->
-    <?php endif; ?>
+    <?php endif; */?>
 
   </div></div> <!-- /#main, /#main-wrapper -->
 
-  <?php if ($page['triptych_first'] || $page['triptych_middle'] || $page['triptych_last']): ?>
+  <?php /*if ($page['triptych_first'] || $page['triptych_middle'] || $page['triptych_last']): ?>
     <div id="triptych-wrapper"><div id="triptych" class="clearfix">
       <?php print render($page['triptych_first']); ?>
       <?php print render($page['triptych_middle']); ?>
       <?php print render($page['triptych_last']); ?>
     </div></div> <!-- /#triptych, /#triptych-wrapper -->
-  <?php endif; ?>
+  <?php endif; */?>
 
   <div id="footer-wrapper"><div class="section">
 
-    <?php if ($page['footer_firstcolumn'] || $page['footer_secondcolumn'] || $page['footer_thirdcolumn'] || $page['footer_fourthcolumn']): ?>
+    <?php /*if ($page['footer_firstcolumn'] || $page['footer_secondcolumn'] || $page['footer_thirdcolumn'] || $page['footer_fourthcolumn']): ?>
       <div id="footer-columns" class="clearfix">
         <?php print render($page['footer_firstcolumn']); ?>
         <?php print render($page['footer_secondcolumn']); ?>
         <?php print render($page['footer_thirdcolumn']); ?>
         <?php print render($page['footer_fourthcolumn']); ?>
       </div> <!-- /#footer-columns -->
-    <?php endif; ?>
+    <?php endif; */?>
 
-    <?php if ($page['footer']): ?>
+    <?php /*if ($page['footer']): ?>
       <div id="footer" class="clearfix">
         <?php print render($page['footer']); ?>
       </div> <!-- /#footer -->
-    <?php endif; ?>
+    <?php endif; */?>
 
   </div></div> <!-- /.section, /#footer-wrapper -->
 
 </div></div> <!-- /#page, /#page-wrapper -->
+<?php include 'page.footer.inc'; ?>
+
